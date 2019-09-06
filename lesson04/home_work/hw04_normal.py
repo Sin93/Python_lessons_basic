e788eb526b3fe9633bee8a5fca7ec1c6a1e09786
Sin93 <zinovev.maxim93@mail.ru>
2019-09-06 03:29:42 +0300

    Урок 4

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -1,5 +1,8 @@
-
-# После отпуска (3-4 сентября) сдам, сейчас никак не успеваю(
+import re
+import string
+import os
+import random
+import pickle
 
 # Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
@@ -22,6 +25,23 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+
+newLine = ''
+listLines = []
+for let in line:
+    if let in string.ascii_lowercase:
+        newLine = newLine + let
+    else:
+        if newLine == '':
+            continue
+        else:
+            listLines.append(newLine)
+            newLine = ''
+listLines.append(newLine)
+print('Задача 1:\nВариант исполнения без re: \n', listLines)
+
+pattern1 = '[a-z]+'
+print('Вариант с re: \n', re.findall(pattern1, line))
 
 
 # Задание-2:
@@ -48,9 +68,62 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+newLine = ''
+listLines2 = []
+i = 2
+while i <= len(line_2) - 3:
+    if line_2[i-2] in string.ascii_lowercase and line_2[i-1] in string.ascii_lowercase and\
+            line_2[i] in string.ascii_uppercase and line_2[i+1] in string.ascii_uppercase and\
+            line_2[i+2] in string.ascii_uppercase:
+        newLine = newLine + line_2[i]
+        i += 1
+        while True:
+            if line_2[i] in string.ascii_uppercase and line_2[i+1] in string.ascii_uppercase and\
+                    line_2[i+2] in string.ascii_uppercase:
+                newLine = newLine + line_2[i]
+                i += 1
+            else:
+                listLines2.append(newLine)
+                newLine = ''
+                i += 1
+                break
+    else:
+        newLine = ''
+        i += 1
+print('\nЗадача 2: \nВариант без re: \n', listLines2)
+
+pattern2 = r'[a-z]{2}[A-Z]+[A-Z]{2}'
+list2 = re.findall(pattern2, line_2)
+list3 = [itm[2:-2] for itm in list2]
+print('Вариант с re: \n', list3)
+
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+with open(os.path.join('files', '2500 numbers.txt'), 'wb') as f:
+    a = random.randint(10**2499, (10**2500)-1)
+    a = str(a)
+    pickle.dump(a, f)
+with open(os.path.join('files', '2500 numbers.txt'), 'rb') as f:
+    reed_data = pickle.load(f)
+    print('Произвольное 2500-значное число:', reed_data, '\nСамые длинные последовательности цифр по каждому числу:')
+    maxLine = ['']
+    i = 0
+    while i < 10:
+        pattern3 = i
+        pattern3 = str(pattern3) + '+'
+        numList = re.findall(pattern3, reed_data)
+        numList.sort()
+        print(numList[-1])
+        if len(maxLine[0]) < len(numList[-1]):
+            maxLine = [numList[-1]]
+        elif len(maxLine[0]) == len(numList[-1]):
+            maxLine.append(numList[-1])
+        i += 1
+    print('\nЗадача 3:\nСамая длинная последовательность цифр в случайном 2500-значном числе, состоит из',\
+          len(maxLine[0]), 'цифр\nКоличество таких последовательностей:', len(maxLine), '\nСписок последовательностей:', maxLine)
