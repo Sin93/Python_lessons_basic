f712b310dae2062e0b10d50d880ce279ef342b11
Sin93 <zinovev.maxim93@mail.ru>
2019-09-06 13:06:42 +0300

    ДЗ 5

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -1,10 +1,73 @@
+import os
+import sys
 # Задача-1:
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+def createDIR(dirrectory, name):
+    try:
+        path = f'{dirrectory}\{name}'
+        os.mkdir(path)
+        print(f'Дирректория {name} создана')
+        return True
+    except Exception as e:
+        print(f'Создать дирректорию {name} не вышло, ошибка: ', e)
+        return False
+
+def deleteDIR(name):
+    try:
+        os.rmdir(os.path.join(os.getcwd(), name))
+        print(f'Дирректория {name} удалена')
+        return True
+    except Exception as e:
+        print(f'Удалить дирректорию {name} не вышло, ошибка: ', e)
+        return False
+
+# i = 1
+# while i <= 9:
+#     nameDir = (f'dir_{i}')
+#     a = createDIR(nameDir)
+#     i += 1
+#
+# userAnswer = input('Хотите удалить дирректории dir_1 - dir_9? (y/n)')
+# if userAnswer == 'y':
+#     i = 1
+#     while i <= 9:
+#         nameDir = (f'dir_{i}')
+#         a = deleteDIR(nameDir)
+#         i += 1
+
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
+def dirInThisDIR(dir):
+    try:
+        a = []
+        [a.append(itm) for itm in os.listdir(dir) if os.path.isdir(f'{dir}\{itm}')]
+        return a
+    except Exception as e:
+        return e
+
+def filesInThisDIR(dir):
+    try:
+        a = []
+        [a.append(f) for f in os.listdir(dir)]
+        return a
+    except Exception as e:
+        return e
+
+# dirInThisDIR(os.getcwd())
+
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
+def copyFile(fileName):
+    with open(os.path.join(os.getcwd(), fileName), 'br') as f:
+        reed_data = f.read()
+    with open(os.path.join(os.getcwd(), f'copy-{fileName}'), 'bw') as f:
+        f.write(reed_data)
+
+# copyFile(os.path.basename(sys.argv[0]))
+
--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -1,3 +1,5 @@
+import os
+import hw05_easy as myFunc
 # Задача-1:
 # Напишите небольшую консольную утилиту,
 # позволяющую работать с папками текущей директории.
@@ -13,3 +15,39 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+dirrectory = os.getcwd()
+while True:
+    print('Текущая дирректория: ', dirrectory)
+    userAnswer = input('''Привет пользователь!
+    Выбери действие:
+    1. Перейти в папку
+    2. Просмотреть содержимое текущей папки
+    3. Удалить папку
+    4. Создать папку
+    5. Вернуться в корневую папку
+    q - завершить программу''')
+    if userAnswer == '1':
+        dirList = myFunc.dirInThisDIR(dirrectory)
+        userAnswer = input(f'В дирректории доступны папки для перехода: {dirList}')
+        if userAnswer in dirList:
+            dirrectory = dirrectory + f'\{userAnswer}'
+            print(f'Переход в папку {userAnswer} успешно выполнен')
+        else:
+            print('Ошибка! Не верное имя папки, повторите ввод')
+    elif userAnswer == '2':
+        print('В текущей дирректории содержатся файлы и папки:', myFunc.filesInThisDIR(dirrectory))
+    elif userAnswer == '3':
+        dirList = myFunc.dirInThisDIR(dirrectory)
+        userAnswer = input(f'В дирректории доступны папки для удаления: {dirList}')
+        myFunc.deleteDIR(dirrectory, userAnswer)
+    elif userAnswer == '4':
+        userAnswer = input('Введите название папки: ')
+        myFunc.createDIR(dirrectory, userAnswer)
+    elif userAnswer == '5':
+        dirrectory = os.getcwd()
+    elif userAnswer == 'q':
+        print('Программа завершена.')
+        break
+    else:
+        print('Неизвестная команда, повторите ввод')
